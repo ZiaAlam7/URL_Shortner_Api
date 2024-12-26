940c6a74b87c27d5e458713853e02023b93588f1
ZiaAlam7 <ziaalam1446@gmail.com>
2024-12-26 16:06:47 +0500

    Third Commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import './Navbar_Responsive.css'
+import logo from '../../../images/logo.svg'
+import menu_icon from '../../../images/menu.svg'
 
 
 const Navbar = () => {
@@ -17,13 +19,13 @@ const Navbar = () => {
     <div className="nav">
 
       <div className='menu'>
-        <img src="../../../images/logo.svg" alt="" />
-        <img src="../../../images/menu.svg" alt="" onClick={handleMenu}/>
+        <img src={logo} alt="" />
+        <img src={menu_icon} alt="" onClick={handleMenu}/>
       </div>
       <div className="navbar" style={{display:menu}}>
 
         <div className="nav_tabs">
-          <img src="../../../images/logo.svg" alt="" />
+          <img src={logo} alt="" />
           <a href="">Features</a>
           <a href="">Pricing</a>
           <a href="">Resources</a>
